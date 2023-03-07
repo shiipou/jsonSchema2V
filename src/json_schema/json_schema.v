0938f91de71c16affb26123a8e55855edca00fce
shiipou <shiishii@nocturlab.fr>
2023-03-07 20:42:59 +0100

    save before wipe

--- src/json_schema/json_schema.v
+++ src/json_schema/json_schema.v
@@ -1,7 +1,7 @@
 module json_schema
 
 import os
-import x.json2
+import json
 
 pub struct JSONSchema {
 pub mut:
@@ -15,38 +15,11 @@ pub mut:
 	additional_properties bool [json: "additionalProperties"]
 }
 
-fn (s JSONSchema) from_json(data json2.Any) {
-	s.schema = data["\$schema"].string() or { "" }
-	s.id = data["\$id"].string() or { "" }
-	s.title = data["title"].string() or { "" }
-	s.description = data["description"].string() or { "" }
-	s.type_ = data["type"].string() or { "" }
-	s.properties = map[string]JSONSchemaPropertyOrRef{}
-	for k, v in data["properties"].object() {
-		if '\$ref' in v.object() {
-			s.properties[k] = JSONSchemaRef{}
-			s.properties[k].from_json(v)
-		} else {
-			s.properties[k] = JSONSchemaProperty{}
-			s.properties[k].from_json(v)
-		}
-	}
-	s.required = data["required"].array() or { [] }
-	s.additional_properties = data["additionalProperties"].bool() or { false }
-}
-
-pub fn (s JSONSchema) to_json() string {
-	return json2.encode(s)
-}
-
 pub fn load_from_file(path string) !JSONSchema {
 	data := os.read_file(path) or { panic(err) }
 	return load_from_string(data)
 }
 
 pub fn load_from_string(data string) !JSONSchema {
-	json_data = json2.raw_decode(data) or { panic(err) }
-	json_schema := JSONSchema{}
-	json_schema.from_json(json_data)
-	return json_schema
+	return json.decode(JSONSchema, data) or { panic(err) }
 }
--- src/json_schema/json_schema_property.v
+++ src/json_schema/json_schema_property.v
@@ -1,20 +1,7 @@
 module json_schema
 
-import x.json2
-
 pub type JSONSchemaType = string | bool | f32 | []JSONSchemaType | map[string]JSONSchemaType | JSONSchemaRef
 pub type JSONSchemaPropertyOrRef = JSONSchemaProperty | JSONSchemaRef
-
-fn (j JSONSchemaPropertyOrRef) from_json(data json2.Any) {
-	json_data := data.as_map()
-	if '\$ref' in json_data {
-		json_schema_ref := j as JSONSchemaRef
-		json_schema_ref.from_json(data)
-	} else {
-		json_schema_property := j as JSONSchemaProperty
-		json_schema_property.from_json(data)
-	}
-}
 
 pub struct JSONSchemaProperty {
 pub mut:
@@ -26,42 +13,7 @@ pub mut:
 	children 		map[string]JSONSchemaProperty			= {}
 }
 
-fn (mut s JSONSchemaProperty) from_json(data json2.Any) {
-	json_data := data.as_map()
-	s.type_ = json_data["type"].str() or { "string" }
-	s.description = json_data["description"].str()
-	items := json_data["items"].arr()
-	for item in items {
-		s.items << JSONSchemaProperty{}
-		s.items.last().from_json(item)
-	}
-	enum_ := json_data["enum"].arr()
-	for item in enum_ {
-		s.enum_ << item
-	}
-	s.is_default = json_data["default"].bool()
-	children := json_data["children"].as_map()
-	for key, value in children {
-		s.children[key] = JSONSchemaProperty{}
-		s.children[key].from_json(value)
-	}
-}
-
-fn (s JSONSchemaProperty) to_json() json2.Any {
-	return json2.encode(s)
-}
-
-
 pub struct JSONSchemaRef {
 pub mut:
 	ref 			string [json: "\$ref"]
-}
-
-fn (mut s JSONSchemaRef) from_json(data json2.Any) {
-	json_data := data.as_map()
-	s.ref = json_data["\$ref"].str()
-}
-
-fn (s JSONSchemaRef) to_json() json2.Any {
-	return json2.encode(s)
 }
